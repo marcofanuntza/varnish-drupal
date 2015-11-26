dbafcd28f8598767b33ca1e4d4259ea2beeb62a0
marcofanuntza <marco.fanuntza@gmail.com>
2015-11-26 20:04:21 +0100

    Modified now ok!

--- start.sh
+++ start.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-service apache2 start && service varnish start
+apache2ctl start
+set -e
+
+exec bash -c \
+  "exec varnishd -F -u varnish \
+  -f $VCL_CONFIG \
+  -s malloc,$CACHE_SIZE \
+  $VARNISHD_PARAMS"
+
